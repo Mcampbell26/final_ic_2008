e9359a6fe5932ff1e3ed184a23a57e7fba9fae41
Megan Campbell <megan.campbell26@yahoo.com>
2020-09-23 09:35:20 -0600

    Add recipe class, fix test

--- test/recipe_test.rb
+++ test/recipe_test.rb
@@ -8,6 +8,6 @@ class RecipeTest < Minitest::Test
   def test_it_exists
     recipe1 = Recipe.new("Mac and Cheese")
 
-    assert_instance_of recipe1, Recipe
+    assert_instance_of Recipe, recipe1
   end
 end
