323002cc5e2f8f6ce33ff89d6d9a22ef3ab38024
Megan Campbell <megan.campbell26@yahoo.com>
2020-09-23 09:38:22 -0600

    Test attributes

--- test/recipe_test.rb
+++ test/recipe_test.rb
@@ -10,4 +10,14 @@ class RecipeTest < Minitest::Test
 
     assert_instance_of Recipe, recipe1
   end
+
+  def test_it_has_attributes
+    recipe1 = Recipe.new("Mac and Cheese")
+
+    assert_equal "Mac and Cheese", recipe1.name
+
+    expected = {}
+
+    assert_equal expected, recipe1.ingredients_required
+  end
 end
