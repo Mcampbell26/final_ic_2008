fe58112dabee565875b5799d7c27d8c8a3cfd0fa
Megan Campbell <megan.campbell26@yahoo.com>
2020-09-23 10:54:41 -0600

    Add cookbook ingredients method

--- lib/cook_book.rb
+++ lib/cook_book.rb
@@ -8,4 +8,15 @@ class CookBook
   def add_recipe(recipe)
     @recipes << recipe
   end
+
+  def ingredients
+    books_ingredients = []
+    @recipes.each do |recipe|
+      recipe.ingredients_required.each do |ingredient|
+        # require 'pry'; binding.pry
+        books_ingredients << ingredient[0].name
+      end
+    end
+    books_ingredients.uniq
+  end
 end
