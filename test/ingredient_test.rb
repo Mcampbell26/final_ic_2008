39c5fa8930d6cf2c8ed9dd96ee52a0d38542bd67
Megan Campbell <megan.campbell26@yahoo.com>
2020-09-23 09:14:00 -0600

    Initial commit

---
+++ test/ingredient_test.rb
@@ -0,0 +1,20 @@
+require './lib/ingredient'
+require 'minitest/autorun'
+require 'minitest/pride'
+
+class IngredientTest < Minitest::Test
+
+  def test_it_exists
+    ingredient1 = Ingredient.new({name: "Cheese", unit: "oz", calories: 50})
+
+    assert_instance_
+  end
+
+  def test_it_has_attributes
+    ingredient1 = Ingredient.new({name: "Cheese", unit: "oz", calories: 50})
+
+    assert_equal "Cheese", ingredient1.name
+    assert_equal "oz", ingredient1.unit
+    assert_equal 50, ingredient1.calories
+  end
+end
